38945d88a4e30c57021dc7138a170f6ce1a6d342
kpilszak <klaudia.pilszak@gmail.com>
2022-01-29 16:22:44 +0100

    Add Spring Cloud Config Server

---
+++ currency-converter-spring-cloud-config-server/src/test/java/com/kpilszak/currencyconverter/currencyconverterspringcloudconfigserver/CurrencyConverterSpringCloudConfigServerApplicationTest.java
@@ -0,0 +1,13 @@
+package com.kpilszak.currencyconverter.currencyconverterspringcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CurrencyConverterSpringCloudConfigServerApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+	
+}
