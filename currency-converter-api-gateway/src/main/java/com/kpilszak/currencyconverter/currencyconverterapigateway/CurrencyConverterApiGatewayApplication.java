6f411c18f0de64f7ab30a23ae3ac394db70f4eae
kpilszak <klaudia.pilszak@gmail.com>
2022-02-02 14:13:27 +0100

    Add api-gateway service

---
+++ currency-converter-api-gateway/src/main/java/com/kpilszak/currencyconverter/currencyconverterapigateway/CurrencyConverterApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.kpilszak.currencyconverter.currencyconverterapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyConverterApiGatewayApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyConverterApiGatewayApplication.class, args);
+	}
+	
+}
---
+++ currency-converter-api-gateway/src/test/java/com/kpilszak/currencyconverter/currencyconverterapigateway/CurrencyConverterApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kpilszak.currencyconverter.currencyconverterapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConverterApiGatewayApplicationTests {
+	
+	@Test
+	void contextLoads() {
+	}
+	
+}
