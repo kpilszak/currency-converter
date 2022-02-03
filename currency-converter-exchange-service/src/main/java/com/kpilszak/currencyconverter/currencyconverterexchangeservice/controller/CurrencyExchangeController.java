e8c84506c157ab6558b273c4d1251c24f1e8a260
kpilszak <klaudia.pilszak@gmail.com>
2022-02-03 11:22:26 +0100

    Prepare for Docker

--- currency-converter-exchange-service/src/main/java/com/kpilszak/currencyconverter/currencyconverterexchangeservice/controller/CurrencyExchangeController.java
+++ currency-converter-exchange-service/src/main/java/com/kpilszak/currencyconverter/currencyconverterexchangeservice/controller/CurrencyExchangeController.java
@@ -2,6 +2,8 @@ package com.kpilszak.currencyconverter.currencyconverterexchangeservice.controll
 
 import com.kpilszak.currencyconverter.currencyconverterexchangeservice.bean.CurrencyExchange;
 import com.kpilszak.currencyconverter.currencyconverterexchangeservice.repository.CurrencyExchangeRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CurrencyExchangeController {
+	
+	Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
 	
 	@Autowired
 	private CurrencyExchangeRepository repository;
@@ -22,6 +26,8 @@ public class CurrencyExchangeController {
 			@PathVariable String from,
 			@PathVariable String to
 	) {
+		logger.info("retrieveExchangeValue called with {} to {}", from, to);
+		
 		CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
 		if (currencyExchange == null) {
 			throw new RuntimeException("Unable to find data for " + from + " to " + to);
